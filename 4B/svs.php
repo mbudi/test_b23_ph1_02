89cac4876b8b24d36ba9fdbe73b6c38ffa55cd94
mbudi <82695909+mbudi@users.noreply.github.com>
2021-04-24 21:03:09 +0700

    Update svs.php

--- 4B/svs.php
+++ 4B/svs.php
@@ -1,5 +1,5 @@
 <?php
-	$servername = "localhost";
+    $servername = "localhost";
 	$username	= "root";
 	$password	= "";
 	$database	= "db_dumb_library";
@@ -7,78 +7,116 @@
 	mysql_select_db($database);
 
 	if(isset($_POST['function']) && !empty($_POST['function'])) {
-		switch($_POST['function']) {
-			case 'writer_get' :
-				writer_get();
-				break;
-			case 'writer_insert' :
-				writer_insert($_POST['name']);
-				break;
-			case 'writer_update' :
-				writer_update($_POST['id'], $_POST['name']);
-				break;
-			case 'writer_delete' :
-				writer_delete($_POST['id']);
-				break;
-			case 'category_get' :
-				category_get();
-				break;
-			case 'category_insert' :
-				category_insert($_POST['name']);
-				break;
-			case 'category_update' :
-				category_update($_POST['id'], $_POST['name']);
-				break;
-			case 'category_delete' :
-				category_delete($_POST['id']);
-				break;
-			case 'book_get' :
-				$id = null;
-				if(isset($_POST['id']) && !empty($_POST['id'])) $id = $_POST['id'];
-				book_get($id);
-				break;
-			case 'book_insert' :
-				book_insert($_POST['name'], $_POST['category_id'], $_POST['writer_id'], $_POST['publication_year'], $_POST['img']);
-				break;
-			case 'book_update' :
-				book_update($_POST['id'], $_POST['name'], $_POST['category_id'], $_POST['writer_id'], $_POST['publication_year'], $_POST['img']);
-				break;
-			case 'book_delete' :
-				book_delete($_POST['id']);
-				break;
-	    }
+    	switch($_POST['function']) {
+    		case 'writer_get' :
+    			$id = null;
+    			if(isset($_POST['id']) && !empty($_POST['id'])) $id = $_POST['id'];
+    			writer_get($id);
+    			break;
+    		case 'writer_insert' :
+    			$table	= "tb_writer";
+    			$col	= "name";
+    			$dt 	= "'".$_POST['name']."'";
+    			insert($table, $col, $dt);
+    			break;
+    		case 'writer_update' :
+    			$table	= "tb_writer";
+    			$dt 	= "name='".$_POST['name']."'";
+    			$wh 	= "id='".$_POST['id']."'";
+    			update($table, $dt, $wh);
+    			break;
+    		case 'writer_delete' :
+				$table	= "tb_writer";
+    			$wh 	= "id='".$_POST['id']."'";
+    			delete($table, $wh);
+    			break;
+    		case 'category_get' :
+    			$id = null;
+    			if(isset($_POST['id']) && !empty($_POST['id'])) $id = $_POST['id'];
+    			category_get($id);
+    			break;
+    		case 'category_insert' :
+    			$table	= "tb_category";
+    			$col	= "name";
+    			$dt 	= "'".$_POST['name']."'";
+    			insert($table, $col, $dt);
+    			break;
+    		case 'category_update' :
+    			$table	= "tb_category";
+    			$dt 	= "name='".$_POST['name']."'";
+    			$wh 	= "id='".$_POST['id']."'";
+    			update($table, $dt, $wh);
+    			break;
+    		case 'category_delete' :
+    			$table	= "tb_category";
+    			$wh 	= "id='".$_POST['id']."'";
+    			delete($table, $wh);
+    			break;
+    		case 'book_get' :
+    			$id = null;
+    			if(isset($_POST['id']) && !empty($_POST['id'])) $id = $_POST['id'];
+    			book_get($id);
+    			break;
+    		case 'book_insert' :
+    			$table	= "tb_book";
+    			$col	= "name, category_id, writer_id, publication_year, img";
+    			$dt 	= "'".$_POST['name']."', '".$_POST['category_id']."', '".$_POST['writer_id']."', '".$_POST['publication_year']."', '".$_POST['img']."'";
+    			insert($table, $col, $dt);
+    			break;
+    		case 'book_update' :
+    			$table	= "tb_book";
+    			if(isset($_POST['name']) && !empty($_POST['name'])) {
+    				$dt = "name='".$_POST['name']."'";
+    			}
+    			if(isset($_POST['category_id']) && !empty($_POST['category_id'])) {
+    				$dt = "category_id='".$_POST['category_id']."'";
+    			}
+    			if(isset($_POST['writer_id']) && !empty($_POST['writer_id'])) {
+    				$dt = "writer_id='".$_POST['writer_id']."'";
+    			}
+    			if(isset($_POST['publication_year']) && !empty($_POST['publication_year'])) {
+    				$dt = "publication_year='".$_POST['publication_year']."'";
+    			}
+    			if(isset($_POST['img']) && !empty($_POST['img'])) {
+    				$dt = "img='".$_POST['img']."'";
+    			}
+    			$wh 	= "id='".$_POST['id']."'";
+    			update($table, $dt, $wh);
+    			break;
+    		case 'book_delete' :
+    			$table	= "tb_book";
+    			$wh 	= "id='".$_POST['id']."'";
+    			delete($table, $wh);
+    			break;
+        }
+    }
+
+	function writer_get($id = null) {
+		$wh 	= "";
+		if (!is_null($id)){
+			$wh = "WHERE id=".$id;
+		}
+		$sql	= "SELECT * FROM tb_writer".$wh;
+		$query	= mysql_query($sql);
+		while($r= mysql_fetch_assoc($query)) {
+			$data[] = $r;
+		}
+		mysql_close();
+		echo json_encode($data);
 	}
 
-	function writer_get() {
-
-	}
-
-	function writer_insert() {
-		
-	}
-
-	function writer_update() {
-		
-	}
-
-	function writer_delete() {
-		
-	}
-
-	function category_get() {
-
-	}
-
-	function category_insert() {
-		
-	}
-
-	function category_update() {
-		
-	}
-
-	function category_delete() {
-		
+	function category_get($id = null) {
+		$wh 	= "";
+		if (!is_null($id)){
+			$wh = "WHERE id=".$id;
+		}
+		$sql	= "SELECT * FROM tb_category".$wh;
+		$query	= mysql_query($sql);
+		while($r= mysql_fetch_assoc($query)) {
+			$data[] = $r;
+		}
+		mysql_close();
+		echo json_encode($data);
 	}
 
 	function book_get($id = null) {
@@ -102,15 +140,24 @@
 		echo json_encode($data);
 	}
 
-	function book_insert() {
-		
+	function insert($table, $col, $dt) {
+		$sql	= "INSERT INTO ".$table." (".$col.") VALUES (".$dt.")";
+		mysql_query($sql);
+		mysql_close();
+		echo json_encode(true);
 	}
 
-	function book_update() {
-		
+	function update($table, $dt, $wh) {
+		$sql	= "UPDATE ".$table." SET ".$dt." WHERE ".$wh;
+		mysql_query($sql);
+		mysql_close();
+		echo json_encode(true);
 	}
 
-	function book_delete() {
-		
+	function delete($table, $wh) {
+		$sql	= "DELETE FROM ".$table." WHERE ".$wh;
+		mysql_query($sql);
+		mysql_close();
+		echo json_encode(true);
 	}
 ?>
